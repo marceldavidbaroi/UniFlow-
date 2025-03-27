c552e6510750cd551c50b82afa04e88c7c95a453
David Baroi <marceldavidbaroi@gmail.com>
2025-03-27 22:28:04 +0600

    store structure changed

--- src/stores/auth-store/index.js
+++ src/stores/auth-store/index.js
@@ -1,18 +1,19 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
-import { auth } from 'boot/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { registerUser, loginUser, logoutUser } from './action'; // Fix import
+// import { auth } from 'boot/firebase';
+// import { onAuthStateChanged } from 'firebase/auth';
+import actions from './action'; // Fix import
 
-export const useAuthStore = defineStore('auth', () => {
-  const user = ref(null);
-  const isAuthenticated = ref(false);
-
-  // Monitor authentication state
-  onAuthStateChanged(auth, (firebaseUser) => {
-    user.value = firebaseUser;
-    isAuthenticated.value = !!firebaseUser;
-  });
-
-  return { user, isAuthenticated, registerUser, loginUser, logoutUser }; // Return functions separately
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    isAuthenticated: false,
+  }),
+  actions,
+  persist: {
+    key: 'auth',
+    pick: ['user', 'isAuthenticated'],
+  },
+  created() {
+    this.initAuthState();
+  },
 });
