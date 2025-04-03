1078a3ae6ad8d8e45d3f0bc481e21f1d4fb3ca3f
David Baroi <marceldavidbaroi@gmail.com>
2025-04-03 13:36:24 +0600

    default to /home

--- src/modules/home/routes.js
+++ src/modules/home/routes.js
@@ -1,5 +1,9 @@
 export default [
   {
+    path: '/',
+    redirect: '/home' // Redirect root path to /home
+  },
+  {
     path: '/home',
     component: () => import('src/layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('./pages/HomeIndex.vue') }],
