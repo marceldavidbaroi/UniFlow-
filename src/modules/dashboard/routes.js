a41f75acf6f31e65c88e2009b5c24f98c2658dd3
David Baroi <marceldavidbaroi@gmail.com>
2025-04-06 19:20:29 +0600

    dashboard route gard added

--- src/modules/dashboard/routes.js
+++ src/modules/dashboard/routes.js
@@ -3,8 +3,18 @@ export default [
     path: '/dashboard',
     component: () => import('src/layouts/MainLayout.vue'),
     children: [
-      { path: 'student', component: () => import('./pages/DashboardStudentIndev.vue') },
-      { path: 'teacher', component: () => import('./pages/DashboardTeacherIndex.vue') },
+      { path: 'student', component: () => import('./pages/DashboardStudentIndev.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['student']
+        }
+       },
+      { path: 'teacher', component: () => import('./pages/DashboardTeacherIndex.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['teacher']
+        }
+       },
       { path: 'admin', component: () => import('./pages/DashboardAdminIndex.vue') },
     ],
   },
