7e11fbaae61a60a182628588a7eb2127b0ea5ed6
David Baroi <marceldavidbaroi@gmail.com>
2025-04-06 19:18:18 +0600

    group route gard added

--- src/modules/group/routes.js
+++ src/modules/group/routes.js
@@ -3,10 +3,34 @@ export default [
     path: '/group',
     component: () => import('src/layouts/MainLayout.vue'),
     children: [
-      { path: 'create', component: () => import('./pages/CreateGroupIndex.vue') },
-      { path: 'list', component: () => import('./pages/GroupListIndex.vue') },
-      { path: ':id', component: () => import('./pages/GroupDetailsIndex.vue') },
-      { path: 'join/:id', component: () => import('./pages/GroupJoinIndex.vue') },
+      {
+        path: 'create',
+        component: () => import('./pages/CreateGroupIndex.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['teacher'],
+        },
+      },
+      {
+        path: 'list',
+        component: () => import('./pages/GroupListIndex.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['teacher','student'],
+        },
+      },
+      { path: ':id', component: () => import('./pages/GroupDetailsIndex.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['teacher','student']
+        }
+       },
+      { path: 'join/:id', component: () => import('./pages/GroupJoinIndex.vue'),
+        meta: {
+          requiresAuth: true,
+          roles: ['teacher','student']
+        }
+       },
     ],
   },
 ]
